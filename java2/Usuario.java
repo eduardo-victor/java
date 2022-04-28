c3aaf34ba5040b54d49870af291a6763c85acba1
Víctor Eduardo <100293881+eduardo-victor@users.noreply.github.com>
2022-04-28 16:19:40 -0300

    Create Usuario.java

---
+++ java2/Usuario.java
@@ -0,0 +1,19 @@
+public class Usuario {
+    private String nome;
+    private String CPF;
+    private String x;
+    private String y;
+
+    public Usuario(){
+
+    }
+
+    public Usuario(String nome, String CPF, String x, String y) {
+        this.nome = nome;
+        this.CPF = CPF;
+        this.x = x;
+        this.y = y;
+    }
+
+
+}
